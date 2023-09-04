177cc53b939213c318c00d4baa5c0b219b7edc21
davinder_0604 <87602553+davindergti9060@users.noreply.github.com>
2023-09-04 12:26:15 -0700

    Update and rename Project: positive numbers to Positive_numbers.py

--- Positive_numbers.py
+++ Positive_numbers.py
@@ -17,3 +17,23 @@ while(num < len(list1)):
 
 OUTPUT:
 12, 5, 64
+
+
+INPUT2:
+# list(list12) of numbers/integers
+list2 = [12, 14, -95, 3]
+num = 0
+ 
+# For performing looping we use while loop
+while(num < len(list2)):
+     
+    # Checking condition using if statement( if num is greater and equals to zero)
+    if list2[num] >= 0:
+        print(list2[num], end = " ")
+        # Printing positive numbers i.e, zero or greater/no negative numbers
+
+    # increment in num
+    num += 1
+
+OUTPUT2:
+12, 14, 3
