ca8708af8722c023f3703535523c82f3ec88af1e
davinder_0604 <87602553+davindergti9060@users.noreply.github.com>
2023-09-07 11:51:53 -0700

    Update and rename most_frequent.py to most_frequent_function.py

--- most_frequent_function.py
+++ most_frequent_function.py
@@ -1,27 +1,27 @@
 Que. Write Python code to create a function called most_frequent that takes a string and prints the letters in decreasing order of frequency. Use dictionaries.
 
 INPUT:
->>>def most_frequent(input_string):
+def most_frequent(input_string):
     # This removes spaces and convert the whole string to lowercase
->>>    input_string = input_string.replace(" ", "").lower()
+    input_string = input_string.replace(" ", "").lower()
     # Initializing an empty dictionary to store letter frequencies
->>>    letter_frequency = {}
+    letter_frequency = {}
     # Counting the frequency of each letter in the string input by user
->>>    for letter in input_string:
->>>        if letter.isalpha():
->>>            if letter in letter_frequency:
->>>                letter_frequency[letter] += 1
->>>            else:
->>>                letter_frequency[letter] = 1
+   for letter in input_string:
+        if letter.isalpha():
+            if letter in letter_frequency:
+                letter_frequency[letter] += 1
+            else:
+                letter_frequency[letter] = 1
     # Sorting the dictionary by values (i.e, frequency) in descending order
->>>    sorted_frequency = sorted(letter_frequency.items(), key=lambda x: x[1], reverse=True)
+    sorted_frequency = sorted(letter_frequency.items(), key=lambda x: x[1], reverse=True)
     # Print the letters in descending order of their frequency
->>>    for letter, frequency in sorted_frequency:
->>>        print(f"{letter}: {frequency}")
+    for letter, frequency in sorted_frequency:
+        print(f"{letter}: {frequency}")
 # Getting string input from the user
->>>user_input = input("Enter a string: ")
+user_input = input("Enter a string: ")
 # Call the function with the user's input
->>>most_frequent(user_input)
+most_frequent(user_input)
 
 OUTPUT:
 # random try:
